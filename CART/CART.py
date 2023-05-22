7d4c32784a86f7bf1ae969e3593b0c2738739805
Aldeena <darkmagician692@gmail.com>
2023-05-22 14:02:57 -0300

    starting fitting function for classification

--- CART/CART.py
+++ CART/CART.py
@@ -1,23 +1,25 @@
-import pandas as pd #Data maniuplation
-import numpy as np #Data manipulation
+import pandas as pd  # Data maniuplation
+import numpy as np  # Data manipulation
 
-from sklearn.model_selection import train_test_split #Splitting data into train and test samples
-from sklearn.metrics import classification_report #Model evaluation metrics
-from sklearn import tree #Decision metrics
+# Splitting data into train and test samples
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report  # Model evaluation metrics
+from sklearn import tree  # Decision metrics
 
-import plotly.express as px #Data visualization
-import plotly.graph_objects as go #Data visualization
-import graphviz #Plotting decision tree graphs
+import plotly.express as px  # Data visualization
+import plotly.graph_objects as go  # Data visualization
+import graphviz  # Plotting decision tree graphs
 
 
 import os
 import sys
 
+
 class CART:
-    def __init__(self, csvFilePath : str) -> None:
+    def __init__(self, csvFilePath: str) -> None:
         pd.options.display.max_columns = 50
 
-        self.file = pd.read_csv(csvFilePath, encoding= 'utf-8')
+        self.file = pd.read_csv(csvFilePath, encoding='utf-8')
 
         print(self.file)
 
@@ -25,6 +27,12 @@ class CART:
         input = self.file[['qPa', 'Pulso', 'Resp']]
         output = self.file['Gravidade'].values
 
+    def fitting(self, input, output criterion, mdepth):
 
+        input_test, input_train, output_test, output_train = train_test_split(
+            input, output, test_size=0.2)
 
-    # def fitting()
+        model = tree.DecisionTreeClassifier(
+            criterion=criterion, max_depth=mdepth)
+
+        classification = model.fit(input_train, output_train)
